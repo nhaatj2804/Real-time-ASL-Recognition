00c63e57c5dc5738bf347f41322065320dd14c31
nhaatj <nguyenngocnhat2004st@gmail.com>
2025-06-10 16:32:05 +0700

    update lstm

--- lstm/lstm_architecture.py
+++ lstm/lstm_architecture.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 class LSTMModel(nn.Module):
     def __init__(self,input_size = 258,hidden_size = 160,output_size = 9):
         super(LSTMModel,self).__init__()
-        self.lstm = nn.LSTM(input_size,hidden_size,num_layers = 1,batch_first = True,bidirectional = True)
+        self.lstm = nn.LSTM(input_size,hidden_size,num_layers = 1,batch_first = True)
         self.dropout = nn.Dropout(0.6)
-        self.fc1 = nn.Linear(hidden_size*2,hidden_size)
+        self.fc1 = nn.Linear(hidden_size,hidden_size)
         self.bn = nn.BatchNorm1d(hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size,output_size)
